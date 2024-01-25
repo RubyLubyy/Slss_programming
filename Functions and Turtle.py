c5d3a32051f1e7d53e5581db2cd1e75dbe67d971
Ruby <rubytangw@gmail.com>
2024-01-25 11:18:00 -0800

    Done turtle

--- Functions and Turtle.py
+++ Functions and Turtle.py
@@ -1,65 +1,110 @@
 # Functions and Turtle
-# Author : Ruby
-# Date : 28th Nov 2023
+# Author: Ubial
+# 28 November 2023
 
 import turtle
 
-Skittles_yellow = turtle
+alfred_river_wilson = turtle.Turtle()
+alfred_river_wilson.color("lightgreen")
+alfred_river_wilson.shape("turtle")
 
-Skittles_yellow = turtle.Turtle
-Skittles_yellow.color("lightgreen")
-Skittles_yellow.shape("turtle")
 
-def squared(num : float) -> float : 
-    """Ruturns the given number squared"""
-    return num **2
+def squared(num: float) -> float:
+    """Returns the given number squared"""
+    return num**2
+
 
 def draw_square(side_length: float) -> None:
-    for _ in range(4) :
-        Skittles_yellow.forward(side_length)
-        Skittles_yellow.left(90)
+    for _ in range(4):
+        alfred_river_wilson.forward(side_length)
+        alfred_river_wilson.left(90)
 
-def draw_tree(level : int, height : int) -> None : 
-    """ A recursive function that draws a tree with inital given height
-    
-    Params : 
+
+def draw_tree(level: int, height: int) -> None:
+    """A recursive function that draws a tree
+    with initial given height
+
+    Params:
+
     level - number representing levels of branches
-    height - height of the main trunk in pixels """
+    height - height of the main trunk in pixels
+    """
 
-    if level > 0 : 
+    if level > 0:
         # 1. Draw the branch
-        Skittles_yellow.forward(height)
+        alfred_river_wilson.forward(height)
 
         # 2. Turn to the left
-        Skittles_yellow.left(39)
+        alfred_river_wilson.left(39)
 
-        # 3. Draw a smaller tree (current level -1)
-        draw_tree(level-1, height/1.5)
-
-        # 4. Turn to the right
-        Skittles_yellow.right(39 * 2)
-
-        # 5. Drawa smaller tree (current level -1)
+        # 3. Draw a smaller tree (current level - 1)
         draw_tree(level - 1, height / 1.5)
 
-        # 6. Move back to the beginning
-        Skittles_yellow.left(39)
-        Skittles_yellow.back(height)
-    else : 
+        # 4. Turn to the right
+        alfred_river_wilson.right(39 * 2)
+
+        # 5. Draw a smaller tree (current level - 1)
+        draw_tree(level - 1, height / 1.5)
+
+        # 6. Move back to beginning
+        alfred_river_wilson.left(39)
+        alfred_river_wilson.back(height)
+    else:
         # create a level 0 tree, which is a leaf
-            original_colour = Skittles_yellow.color()
-            Skittles_yellow.color("green")
-            Skittles_yellow.stamp()
-            Skittles_yellow.color(original_colour[0])
+        original_colour = alfred_river_wilson.color()
+        alfred_river_wilson.color("green")
+        alfred_river_wilson.stamp()
+        alfred_river_wilson.color(original_colour[0])
 
-# Setting Skittles to draw the tree
-# Skittles_yellow.hideturtle()
-Skittles_yellow. setheading(90)
-Skittles_yellow. width(4)
-Skittles_yellow.color("brown")
-Skittles_yellow.shape("arrow")
-Skittles_yellow.speed(200)
 
-draw_tree(10, 150)
+def draw_fancy_tree(level: int, height: int) -> None:
+    """A recursive function that draws a tree
+    with initial given height
+
+    Params:
+
+    level - number representing levels of branches
+    height - height of the main trunk in pixels
+    """
+
+    if level > 0:
+        # 1. Draw the branch
+        alfred_river_wilson.forward(height)
+
+        # 2. Turn to the left
+        alfred_river_wilson.left(39)
+        #  a. Draw a smaller tree (current level - 1)
+        draw_fancy_tree(level - 1, height / 1.5)
+
+        # 3. Draw a smaller tree in the middle
+        alfred_river_wilson.right(39)
+        draw_fancy_tree(level - 1, height / 1.5)
+
+        # 4. Turn to the right
+        alfred_river_wilson.right(39)
+
+        # 5. Draw a smaller tree (current level - 1)
+        draw_fancy_tree(level - 1, height / 1.5)
+
+        # 6. Move back to beginning
+        alfred_river_wilson.left(39)
+        alfred_river_wilson.back(height)
+    else:
+        # create a level 0 tree, which is a leaf
+        original_colour = alfred_river_wilson.color()
+        alfred_river_wilson.color("green")
+        alfred_river_wilson.stamp()
+        alfred_river_wilson.color(original_colour[0])
+
+
+# Setting ARWilson to draw the tree
+alfred_river_wilson.hideturtle()
+alfred_river_wilson.setheading(90)
+alfred_river_wilson.width(4)
+alfred_river_wilson.color("brown")
+alfred_river_wilson.shape("arrow")
+alfred_river_wilson.speed(0)
+
+draw_fancy_tree(4, 150)
 
 turtle.done()
